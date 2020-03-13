05eb7254a8eaf9179dcbb4102cd29e51676f2603
Po <>
2020-03-14 01:40:57 +0800

    update D1

--- Javascript/Day1 - my frist js/j1.js
+++ Javascript/Day1 - my frist js/j1.js
@@ -9,13 +9,13 @@ function reset() { // nhập lại
     }
 }
 function PTb2() {
-    var a = document.getElementById("numA").value;
+    var a = document.getElementById("numA").value; // nhập gí trị của a
     var b = document.getElementById("numB").value;
     var c = document.getElementById("numC").value;
     if (a == 0) {
         if (b == 0) {
             if (c == 0)
-                document.getElementById("PTb2").innerHTML = "Phương trình vô số nghiệm: ";
+                document.getElementById("PTb2").innerHTML /*in ra màn hình */= "Phương trình vô số nghiệm: ";
             else
                 document.getElementById("PTb2").innerHTML = "Phương trình vô nghiệm";
         }
@@ -26,7 +26,7 @@ function PTb2() {
     else {
         const delta = b * b - 4 * a * c;
         if (delta > 0) {
-            var x1 = (Math.sqrt(delta) + b) / (2 * a);
+            var x1 = (Math.sqrt(delta) + b) / (2 * a); // tính toán dùng Math,
             var x2 = (b - Math.sqrt(delta)) / (2 * a);
             document.getElementById("PTb2").innerHTML = "x1 = " + x1 + ", x2 = " + x2;
         }
@@ -41,7 +41,7 @@ function PTb2() {
 function DateofMonth() {
     var a = document.getElementById("month").value;
     var b = document.getElementById("year").value;
-    if (a <= 0 || a > 12 || b < 0) {
+    if (a <= 0 || a > 12 || b <= 0) {
         document.getElementById("error").innerHTML = "Nhập lại tháng!";
     }
     
@@ -75,7 +75,7 @@ function DateofMonth() {
         }
     }
 }
-function check(i){
+function check(i){ // hàm kiểm tra snt
     var dem = 0;
     for(var j = 1; j <= i; j++){
         if(i % j == 0){
@@ -89,16 +89,16 @@ function check(i){
 function PrintPrime(){
     var a = document.getElementById("Num").value
     var b = parseInt(a);
-    var p = [];
+    var Arr = []; // mảng để in ra các snt từ 1 -> n
     if(b < 0){
-        document.getElementById("errorNum").innerHTML = "Vui lòng nhập lại! ";
+        document.getElementById("errorNum").innerHTML = "Nnhập lại n. ";
     }else{
         document.getElementById("errorNum").innerHTML = "";
         for(var i = 1; i <= b; i++){
-            if(test(i) == 1){
-                p.push(i);
+            if(check(i) == 1){
+                Arr.push(i); // gọi hàm dùng "push"
             }
         }
     }
-    document.getElementById("snt").innerHTML = p;
+    document.getElementById("snt").innerHTML = Arr;
 }
